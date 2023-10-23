69fd04a37565a07861737e402042c152eb03b2c2
Ahmed-Mohammed-11 <ahmedmo1162002@gmail.com>
2023-10-23 08:06:43 +0300

    implementation of DFS/BFS completed without testing

--- src/main/java/search_algorithms/FirstSearch.java
+++ src/main/java/search_algorithms/FirstSearch.java
@@ -3,6 +3,7 @@ package search_algorithms;
 import data_type.Queue;
 import data_type.Stack;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -11,40 +12,74 @@ public class FirstSearch<T> extends Search<T>{
         switch (type) {
             case 'B' -> frontier = new Queue<>();
             case 'D' -> frontier = new Stack<>();
+            default -> System.err.println("wrong choice");
         }
 
         explored = new HashSet<>();
         visited = new HashSet<>();
-        parent = new HashMap<>();
+        childParent = new HashMap<>();
+        neighbors = new Neighbors<>();
+        depth = 0 ;
     }
 
     @Override
-    public void search(T initialState, T goalState) {
+    public boolean search(T initialState, T goalState) {
+        //push the initial state in the frontier and to frontier U explored hashset
+        frontier.push(initialState);
+        childParent.put(initialState, initialState);
+        visited.add(initialState);
+        //initialize current state variable
+        T currentState ;
+        while(! frontier.isEmpty())
+        {
+            //start with getting the front value in the frontier
+            currentState = frontier.pop();
+            //add this state to explored hashset and check if we reached the goal state
+            explored.add(currentState);
+            if(goalState.equals(currentState))
+            {
+                return true;
+            }
 
+            //if the current state is not the goal state then get its neighbors and
+            //push them to frontier, frontier U explored hashset and to the parent-child hashmap
+            for(T neighbor : neighbors.getNeighbors(currentState))
+            {
+                if(visited.contains(neighbor))
+                {
+                    frontier.push(neighbor);
+                    childParent.put(neighbor, currentState);
+                    visited.add(neighbor);
+                }
+            }
+        }
+
+        //didn't find the goal state
+        return false;
     }
 
     @Override
-    public T getParent(T state) {
-        return null;
-    }
+    public T getParent(T state) { return childParent.get(state); }
 
     @Override
-    public T getDepth(T state) {
-        return null;
-    }
+    public Integer getDepth() { return depth; }
+
+    //as here the cost of each step is one
+    public Integer getCostOfPath() { return depth ; }
 
     @Override
-    public T getStatesExplored() {
-        return null;
-    }
+    public ArrayList<T> getNodesExpanded() { return new ArrayList<>(explored); }
 
     @Override
-    public T[] getNeighbors(T state) {
-        return null;
-    }
-
-    @Override
-    public T[] getPath(T state) {
-        return null;
+    public ArrayList<T> getPath(T initialState , T goalState ) {
+        T state = goalState ;
+        ArrayList<T> pathToGoal = new ArrayList<>();
+        while (!state.equals(initialState))
+        {
+            pathToGoal.add(state);
+            state = getParent(state);
+        }
+        depth = pathToGoal.size();
+        return new ArrayList<>();
     }
 }
