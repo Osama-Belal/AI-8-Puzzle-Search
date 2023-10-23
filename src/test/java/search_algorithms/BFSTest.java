74b446849098b57c0cdd3e5a8cafedc6520814e5
Ahmed-Mohammed-11 <ahmedmo1162002@gmail.com>
2023-10-23 09:52:34 +0300

    slight modification on instance naming : sorry!

--- src/test/java/search_algorithms/BFSTest.java
+++ src/test/java/search_algorithms/BFSTest.java
@@ -7,15 +7,15 @@ class BFSTest {
 
     @Test
     void SearchAndFindGoal() {
-        var firstSearch = new FirstSearch<>('B');
-        boolean searchResult = firstSearch.search(120345678, 12345678);
+        var bfs = new FirstSearch<>('B');
+        boolean searchResult = bfs.search(120345678, 12345678);
         assertTrue(searchResult);
     }
 
     @Test
     void SearchAndNotFindGoal() {
-        var firstSearch = new FirstSearch<>('B');
-        boolean searchResult = firstSearch.search(123456870, 12345678);
+        var bfs = new FirstSearch<>('B');
+        boolean searchResult = bfs.search(123456870, 12345678);
         assertFalse(searchResult);
     }
 }
