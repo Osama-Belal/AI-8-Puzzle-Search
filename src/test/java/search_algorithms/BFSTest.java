81ab0b70fc2927c9c4f851c8bac0798ec3e0561d
Ahmed-Mohammed-11 <ahmedmo1162002@gmail.com>
2023-10-24 09:57:32 +0300

    BFS Tests (Succeeded)

--- src/test/java/search_algorithms/BFSTest.java
+++ src/test/java/search_algorithms/BFSTest.java
@@ -5,17 +5,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BFSTest {
 
+    FirstSearch bfs = new FirstSearch<>('B');
     @Test
-    void SearchAndFindGoal() {
-        var bfs = new FirstSearch<>('B');
-        boolean searchResult = bfs.search(120345678, 12345678);
+    void SearchAndFindGoal()
+    {
+        boolean searchResult = bfs.search(125340678L, 12345678L);
         assertTrue(searchResult);
+        System.out.println(bfs.getNodesExpanded());
     }
 
     @Test
-    void SearchAndNotFindGoal() {
-        var bfs = new FirstSearch<>('B');
-        boolean searchResult = bfs.search(123456870, 12345678);
+    void SearchAndFindGoalWithIncreasedStepsOne()
+    {
+        boolean searchResult = bfs.search(142658730L, 12345678L);
+        assertTrue(searchResult);
+        System.out.println(bfs.getNodesExpanded());
+    }
+
+    @Test
+    void SearchAndFindGoalWithIncreasedStepsTwo()
+    {
+        boolean searchResult = bfs.search(102754863L, 12345678L);
+        assertTrue(searchResult);
+        System.out.println(bfs.getNodesExpanded());
+    }
+
+    @Test
+    void SearchAndNotFindGoal()
+    {
+        boolean searchResult = bfs.search(123456870L, 12345678L);
         assertFalse(searchResult);
+        System.out.println(bfs.getNodesExpanded());
     }
 }
